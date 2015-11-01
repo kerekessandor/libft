7bf6a184c5b0e8b48b3e7ab127c7042bb9e1e6a7
KerekesSandor <kerekessandor@yahoo.com>
2015-11-01 21:02:53 +0200

    libft

--- libft.h
+++ libft.h
@@ -1,1 +1,15 @@
-szia
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include<unistd.h>
+# include<stdlib.h> 
+
+char	*ft_strdup(const char *s);
+char	*ft_strcpy(char *dest, const char *src);
+char	*ft_strncpy(char *dest, const char *src, size_t n);
+char	*ft_strcat(char *dest, const *src);
+void	ft_putchar(char c);
+void	ft_putstr(char *s);
+int		ft_strlen(const char *s);
+
+#endif
