11b95f448d2f86cf0086981b588c8596016380bb
Sandor KEREKES <skerekes@e1p27.cluj.42.fr>
2015-11-02 16:47:38 +0100

    at academy

--- libft.h
+++ libft.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skerekes <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/11/02 15:40:03 by skerekes          #+#    #+#             */
+/*   Updated: 2015/11/02 16:36:16 by skerekes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef LIBFT_H
 # define LIBFT_H
 
@@ -7,9 +19,12 @@
 char	*ft_strdup(const char *s);
 char	*ft_strcpy(char *dest, const char *src);
 char	*ft_strncpy(char *dest, const char *src, size_t n);
-char	*ft_strcat(char *dest, const *src);
+char	*ft_strcat(char *s1, const char *s2);
+char	*ft_strncat(char *s1, const char *s2, size_t n);
+char	*ft_strchr(const char *s, int c);
 void	ft_putchar(char c);
 void	ft_putstr(char *s);
 int		ft_strlen(const char *s);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
 
 #endif
