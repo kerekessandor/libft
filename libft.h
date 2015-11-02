a35fb9b7b7c525740796d79f3f4fe8149b79985b
Sandor KEREKES <skerekes@e1p27.cluj.42.fr>
2015-11-02 18:54:01 +0100

    at academy

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: skerekes <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/02 15:40:03 by skerekes          #+#    #+#             */
-/*   Updated: 2015/11/02 16:36:16 by skerekes         ###   ########.fr       */
+/*   Updated: 2015/11/02 18:49:17 by skerekes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,16 @@
 char	*ft_strdup(const char *s);
 char	*ft_strcpy(char *dest, const char *src);
 char	*ft_strncpy(char *dest, const char *src, size_t n);
-char	*ft_strcat(char *s1, const char *s2);
+char	*ft_strcat(char *s1, const char *s2); //nu merge bine
 char	*ft_strncat(char *s1, const char *s2, size_t n);
 char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
+char	*ft_strstr(const char *s1, const char *s2); //nu merge
 void	ft_putchar(char c);
 void	ft_putstr(char *s);
 int		ft_strlen(const char *s);
-size_t	ft_strlcat(char *dst, const char *src, size_t size);
+int		ft_strcmp(const char *s1, const char *s2);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);//nu merge deloc
 
 #endif
